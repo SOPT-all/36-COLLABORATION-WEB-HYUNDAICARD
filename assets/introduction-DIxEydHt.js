f1307c56b0fe04bbe45b9ae310f1d6b7d6068a29
gwagjiug <gwagjiug@users.noreply.github.com>
2025-05-19 13:08:03 +0000

    deploy: 3fc31520d7f76baab3dcc43982e50a1fe5af6d58

---
+++ assets/introduction-DIxEydHt.js
@@ -0,0 +1,7 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as o}from"./index-B8rYRX2K.js";import{M as s}from"./index-Bzq-K7dP.js";import"./index-D4lIrffr.js";import"./iframe-BFuzAWfz.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function i(e){const t={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Introduction"}),`
+`,n.jsx(t.h1,{id:"hyundai-card-design-system-cds",children:"Hyundai-Card Design System (CDS)"}),`
+`,n.jsx(t.h3,{id:"the-design-system-for-cyundai-card",children:"The Design System for Cyundai-Card"}),`
+`,n.jsx("br",{}),`
+`,n.jsx(t.h2,{id:"links",children:"Links"}),`
+`,n.jsx(t.p,{children:n.jsx(t.a,{href:"https://github.com/SOPT-all/36-COLLABORATION-WEB-HYUNDAICARD",rel:"nofollow",children:"Github"})}),`
+`,n.jsx(t.h2,{id:"componentsui",children:"Components(ui)"})]})}function p(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(i,{...e})}):i(e)}export{p as default};
